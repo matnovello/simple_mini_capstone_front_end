8ef1c124b77e38cfc70340b0d33148038ffbf3c0
Mat <mvello66@gmail.com>
2021-03-22 22:28:11 -0400

    axios crud

--- products.js
+++ products.js
@@ -5,17 +5,42 @@ var app = new Vue({
   data: function() {
     return {
       message: "Hello from JavaScript!",
-      products:  [],
+      toggleIndex: false,
+      products: [],
+      singleProduct: "",
+      input:  "", 
+      name: "",
+      description: "",
+      price: 0,
     };
   },
   methods: {
     index:  function () {
-      axios
-        .get("http://localhost:3000/api/products")
-        .then(response => {
+      console.log( "hello");
+      axios.get("http://localhost:3000/api/products")
+      .then(response => {
           console.log(response.data);
-          this. = response.data;
+          this.products = response.data;
+          this.toggleIndex = !this.toggleIndex;
         });
+    },
+    showProduct: function (){
+      console.log(parseInt(this.input));
+      axios.get("http://localhost:3000/api/products/" + this.input)
+      .then(response => {
+        this.singleProduct = response.data;
+        
+      });
+    },
+    createProduct: function (){
+      axios.post("http://localhost:3000/api/products", {
+        name: this.name,
+        description:  this.description,
+        price: this.price,
+
+      }).then(response => {
+        console.log(response.data);
+      })
     }
   }
 });
